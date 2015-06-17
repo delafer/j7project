223fe91434476caf90e684f109991cd443c833c1
TavrovsA <weblogic@bk.ru>
2015-06-17 18:44:32 +0200

    fixed bug with sound

--- commontools/xanderView/src/main/java/org/delafer/xanderView/sound/SoundBeep.java
+++ commontools/xanderView/src/main/java/org/delafer/xanderView/sound/SoundBeep.java
@@ -3,11 +3,7 @@ package org.delafer.xanderView.sound;
 import java.io.BufferedInputStream;
 import java.io.InputStream;
 
-import javax.sound.sampled.AudioFormat;
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.Clip;
-import javax.sound.sampled.DataLine;
+import javax.sound.sampled.*;
 
 import net.j7.commons.streams.Streams;
 
@@ -28,26 +24,35 @@ public class SoundBeep {
 //	}
 	public final static void beep() {
 		InputStream is = null;
-		AudioInputStream stream= null;
+//		AudioInputStream stream= null;
 		try {
-			is = new BufferedInputStream(SoundBeep.class.getResourceAsStream("sound01.wav"));
+			is = new BufferedInputStream(SoundBeep.class.getResourceAsStream("/audio/sound01.wav"));
 		    AudioFormat format;
 		    DataLine.Info info;
 		    Clip clip;
 
-		    stream = AudioSystem.getAudioInputStream(is);
+		    final AudioInputStream stream = AudioSystem.getAudioInputStream(is);
 		    format = stream.getFormat();
 		    info = new DataLine.Info(Clip.class, format);
 		    clip = (Clip) AudioSystem.getLine(info);
 		    clip.open(stream);
+		    clip.addLineListener(new LineListener() {
+				public void update(LineEvent le) {
+				    LineEvent.Type type = le.getType();
+				    if (type == LineEvent.Type.CLOSE) {
+				    	Streams.close(stream);
+				    } else if (type == LineEvent.Type.STOP) {
+				      clip.close();
+				    }
+
+				}
+
+		    });
 		    clip.start();
 		}
 		catch (Exception e) {
 			e.printStackTrace();
-		} finally {
-			Streams.close(stream);
 		}
-
 
 	}
 
